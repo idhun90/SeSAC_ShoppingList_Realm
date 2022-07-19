6d39d9488d2c4fe843b8badacd7cd2ae539e4282
idhun90 <idhun90@naver.com>
2022-07-19 14:15:32 +0900

    Shopping 스토리보드, UI테이블 뷰 컨트롤러, UI테이블 셀 컨트롤러 생성

---
+++ Setting/ShoppingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingTableViewCell.swift
+//  TrendMedia
+//
+//  Created by 이도헌 on 2022/07/19.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
